6d74e80618c0792293bbc114e394245e3089c153
codeamac02 <codeamac02@gmail.com>
2016-05-10 13:35:32 -0500

    Creando métodos destructivos

---
+++ destructivos.rb
@@ -0,0 +1,24 @@
+# name = "Fernando"
+
+# puts "Valor de name: #{name}"
+
+# puts "Llamando al método reverse en name : #{name.reverse}"
+
+# puts "Valor de name despues de pasarlo por reverse: #{name}"
+
+# puts "Llamando al método reverse! en name : #{name.reverse!}"
+
+# puts "Valor de name después de pasarlo por reverse!: #{name}"
+
+ciudad = "glasgow"
+
+puts "Mi ciudad favorita es: #{ciudad}"
+
+puts "Llamando al método capitalize en ciudad: #{ciudad.capitalize}"
+
+puts "Valor de ciudad después de pasarlo por capitalize: #{ciudad}"
+
+puts "Llamando al método capitalize! en ciudad: #{ciudad.capitalize!}"
+
+puts "Valor de ciudad después de pasarlo por capitalize!: #{ciudad}"
+
